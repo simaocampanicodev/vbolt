58ba5838b628a812a28fb6a5aceb446d133e89dc
Simão <simaocampanico.dev@gmail.com>
2026-02-19 16:58:01 +0000

    Adding official tracker and twitch icons

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -12,18 +12,16 @@ import { GameRole, UserRole } from '../types';
 import { uploadToCloudinary, uploadBannerToCloudinary } from '../services/cloudinary';
 import { BannerCropModal } from './BannerCropModal';
 
-// Tracker Network oficial logo
+const TRACKER_ICON_URL = "https://trackercdn.com/cdn/tracker.gg/tile-384.png";
+
 const TrackerNetworkIcon = ({ className = "w-6 h-6" }) => (
-  <svg viewBox="0 0 24 24" className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
-  </svg>
+  <img src={TRACKER_ICON_URL} alt="Tracker Network" className={className} style={{ objectFit: 'contain' }} />
 );
 
-// Twitch oficial logo (roxo)
+const TWITCH_ICON_URL = "https://cdn-icons-png.flaticon.com/512/5968/5968819.png";
+
 const TwitchOfficialIcon = ({ className = "w-6 h-6" }) => (
-  <svg viewBox="0 0 24 24" className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path d="M11 2H2v17h6v4l4-4h5l7-7V2h-6v7h-2V2zm8 10l-3 3h-4l-3 3v-3H7V4h12v8z"/>
-  </svg>
+  <img src={TWITCH_ICON_URL} alt="Twitch" className={className} style={{ objectFit: 'contain' }} />
 );
 
 interface BadgeType {
